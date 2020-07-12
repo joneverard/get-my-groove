7420fe515eafe88d525696959e63588edabc8e83
Jon Everard <joneverard@github.com>
2020-07-12 17:45:25 +0100

    add top tracks scope and api route

--- server/lib/auth/auth-rest.js
+++ server/lib/auth/auth-rest.js
@@ -1,11 +1,11 @@
 const passport = require("passport");
 const SpotifyStrategy = require("passport-spotify").Strategy;
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
@@ -18,10 +18,10 @@ passport.use(
     {
       clientID: process.env.APP_KEY,
       clientSecret: process.env.APP_SECRET,
-      callbackURL: "http://localhost:8888/callback"
+      callbackURL: "http://localhost:8888/callback",
     },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-      process.nextTick(function() {
+    function (accessToken, refreshToken, expires_in, profile, done) {
+      process.nextTick(function () {
         // To keep the example simple, the user's spotify profile is returned to
         // represent the logged-in user. In a typical application, you would want
         // to associate the spotify account with a user record in your database,
@@ -41,10 +41,15 @@ module.exports = app => {
   app.get(
     "/auth/spotify",
     passport.authenticate("spotify", {
-      scope: ["user-read-email", "user-read-private", "playlist-read-private"],
-      showDialog: true
+      scope: [
+        "user-top-read",
+        "user-read-email",
+        "user-read-private",
+        "playlist-read-private",
+      ],
+      showDialog: true,
     }),
-    function(req, res) {
+    function (req, res) {
       // The request will be redirected to spotify for authentication, so this
       // function will not be called.
     }
@@ -58,16 +63,16 @@ module.exports = app => {
   app.get(
     "/callback",
     passport.authenticate("spotify", { failureRedirect: "/login" }),
-    function(req, res) {
+    function (req, res) {
       res.redirect("/");
     }
   );
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     res.render("login.html", { user: req.user });
   });
 
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
--- server/lib/user/user-rest.js
+++ server/lib/user/user-rest.js
@@ -1,38 +1,18 @@
 const axios = require("axios");
 const { ensureAuthenticated } = require("../../utils/middleware");
 
-const _ = require("lodash");
-
-// axios.get('https://api.github.com/user', {
-//   headers: {
-//     'Authorization': `token ${access_token}`
-//   }
-// })
-
 module.exports = app => {
   // get list of playlists
   app.get("/user/toptracks", ensureAuthenticated, async function (req, res) {
     console.log(req.session.passport.user.accessToken);
-    const data = axios.get("https://api.spotify.com/v1/me/top/artists", {
-      headers: {
-        Authorization: `token ${req.session.passport.user.accessToken}`,
-      },
-    });
+    const { data } = await axios.get(
+      "https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=short_term",
+      {
+        headers: {
+          Authorization: `Bearer ${req.session.passport.user.accessToken}`,
+        },
+      }
+    );
     res.status(200).send(data);
   });
-  //   app.get("/user/toptracks", ensureAuthenticated, function (req, res) {
-  //     console.log(req.session.passport.user.accessToken);
-  //     spotifyApi.setAccessToken(req.session.passport.user.accessToken);
-  //     spotifyApi
-  //       .getUserPlaylists(req.session.passport.user.id, { limit: 50 })
-  //       .then(({ body }) => {
-  //         const result = {
-  //           ..._.pick(body, ["total", "offset"]),
-  //           playlists: _.map(body.items, item =>
-  //             _.pick(item, ["id", "name", "tracks", "description", "images"])
-  //           ),
-  //         };
-  //         res.status(200).send(result);
-  //       });
-  //   });
 };
